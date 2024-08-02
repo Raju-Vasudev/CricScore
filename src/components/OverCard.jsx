4326ce5f892a1701d6838d4a94e563afc3542d9d
Raju-Vasudev <raju.az@hotmail.com>
2024-08-02 22:50:27 +0530

    refactor

--- src/components/OverCard.jsx
+++ src/components/OverCard.jsx
@@ -1,47 +1,57 @@
 import React from 'react';
-import { ListItem, ListItemText, Chip, Box, Card, CardContent } from '@mui/material';
-import './ScoreBoard.css';
+import { ListItem, ListItemText, Chip, Box, Card, CardContent, Typography } from '@mui/material';
+import './OverCard.css';
 
 const OverItem = ({ index, runsInOver, wicketsInOver, over }) => {
   return (
-    <Card sx={{ marginBottom: '16px', backgroundColor: '#f5f5f5' }}>
+    <Card sx={{ marginBottom: '8px', backgroundColor: '#f5f5f5', padding: '10x' }}>
       <CardContent>
-        <ListItem key={index}>
-          <ListItemText
-            primary={
-              <div className="over-count">
-                <Chip
-                  label={`Over ${index + 1}`}
-                  sx={{
-                    background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
-                    color: 'white',
-                    fontFamily: 'Arial, sans-serif',
-                    width: '175px',
-                    height: '30px',
-                    display: 'flex',
-                    alignItems: 'left',
-                    justifyContent: 'left',
-                    fontSize: '20px',
-                  }}
-                />
-              </div>
-            }
-            secondary={
-              <>
-                <Chip
-                  label={`Runs: ${runsInOver} , Wickets: ${wicketsInOver}`}
-                  sx={{ margin: '6px', marginLeft: '0px', backgroundColor: '#ffeb3b', color: '#000', fontWeight: 'bold' }}
-                />
-                <Box className="over-details" sx={{ display: 'flex', flexWrap: 'wrap' , gap: '4px'}}>
-                  {over.map((delivery, index) => (
-                    <>
-                      <div className="circle">{delivery}</div>
-                    </>
-                  ))}
-                </Box>
-              </>
-            }
-          />
+        <ListItem key={index} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginLeft:'0px'}}>
+          <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: '6px' }}>
+          <Chip
+              label={`Over ${index + 1}`}
+              sx={{
+                background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+                color: 'white',
+                fontFamily: 'Arial, sans-serif',
+                width: '100px',
+                height: '30px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                fontSize: '16px',
+                fontWeight: 'bold',
+                marginRight: '8px',
+                borderRadius: '5px',
+              }}
+            />
+            <Chip
+              label={`Runs: ${runsInOver}`}
+              sx={{
+                backgroundColor: '#ffeb3b',
+                color: '#000',
+                fontWeight: 'bold',
+                fontSize: '14px',
+                marginRight: '8px',
+                borderRadius: '5px',
+              }}
+            />
+            <Chip
+              label={`Wickets: ${wicketsInOver}`}
+              sx={{
+                backgroundColor: '#ffeb3b',
+                color: '#000',
+                fontWeight: 'bold',
+                fontSize: '14px',
+                borderRadius: '5px',
+              }}
+            />
+          </Box>
+          <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems:'center' }}>
+            {over.map((delivery, index) => (
+              <div key={index} className="circle">{delivery}</div>
+            ))}
+          </Box>
         </ListItem>
       </CardContent>
     </Card>
